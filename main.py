6e00aa1b75911c627ebe7ad099e0cd3e432fba79
ZeeshanZeeZx <zeeshanzx26@gmail.com>
2026-04-07 15:03:08 +0530

    add headless config

--- main.py
+++ main.py
@@ -35,8 +35,11 @@ def update_naukri():
     try:
         # 🔹 Chrome options
         options = Options()
-        options.add_argument("--start-maximized")
+        options.add_argument("--headless=new")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--disable-blink-features=AutomationControlled")
+        options.add_argument("--window-size=1920,1080")
 
         # 🔹 Start browser
         driver = webdriver.Chrome(
